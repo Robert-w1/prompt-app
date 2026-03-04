602c31dfb2c6ab54ed9f6fa1b5387084b018df19
pedro <santoslimap3@gmail.com>
2026-03-04 15:18:19 +0800

    commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
-import * as bootstrap from "bootstrap"
-
-// Re-initialize Bootstrap components that require explicit init after Turbo navigations
-document.addEventListener("turbo:load", () => {
-  // Initialize tooltips
-  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((el) => {
-    new bootstrap.Tooltip(el)
-  })
-  // Initialize popovers
-  document.querySelectorAll('[data-bs-toggle="popover"]').forEach((el) => {
-    new bootstrap.Popover(el)
-  })
-})
+import "bootstrap"
