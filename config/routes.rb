fc73c0b744d56fc2b7372bf73649ed4ed0dd1653
Yasmine Fai <yfaingna@gmail.com>
2026-03-06 16:08:38 +0800

    Created search bar in sidebar and logic and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/new'
-  get 'projects/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -15,4 +12,7 @@ Rails.application.routes.draw do
     resources :messages, only: [:create]
   end
   resources :projects, only: [:index, :new, :create, :show]
+
+  # Search route for all (prompts, messages, chats, projects)
+  get 'search', to: 'search#index', as: 'search'
 end
