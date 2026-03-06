b906b5e40808cb7eed154aafb2f15c09e39102ef
Yasmine Fai <yfaingna@gmail.com>
2026-03-06 16:58:17 +0800

    Preliminary changes to chats all

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :chats, only: [:index, :show, :create] do
+  resources :chats, only: [:index, :show, :create, :destroy] do
     resources :messages, only: [:create]
   end
-  
+
   resources :projects, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  
+
   # Search route for all (prompts, messages, chats, projects)
   get 'search', to: 'search#index', as: 'search'
 
